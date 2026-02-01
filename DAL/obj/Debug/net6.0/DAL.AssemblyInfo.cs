9a0237147e905c0d61f57a0668dc4dbebb18a758
Ceyhun Yusif <jeyhun312@gmail.com>
2026-02-01 17:18:17 +0400

    References Added

--- DAL/obj/Debug/net6.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net6.0/DAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1cdb385b251c9a42ee22eb46ee74ed846d9817a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e268973deab85e126bee3ef90294b004877a548e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
